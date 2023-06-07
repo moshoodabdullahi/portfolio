ab477336b784953715f7bf64748ad197abe1358a
Moshood Abdullahi Ayomide <111710949+moshoodabdullahi@users.noreply.github.com>
2023-06-07 11:08:43 +0100

    feat: Add accessibility (#6)
    * feat: Change project title
    
    * fix: Fix prettier error
    
    * feat: Add value and expantiate on color contrast
    
    * fix: Fix prettier error
    
    * style: Add focus pseudo class to the input section
    
    * fix: Fix prettier error
    
    * feat: Add accessibility report file
    
    * chore: Add linter for accessibility
    
    * fix: Fix prttier error
    
    * fix: Fix prettier error
    
    * chore: Update lintr configuration
    
    * feat: fis linter error
    
    * chore: Update linter configuration
    
    * chore: Update linter configuration
    
    * chore: Update linter configuration
    
    * style: Fix prettier error
    
    * docs: Erase non-functional content from project
    
    * docs: Implement mark down syntax to the project
    
    * feat: Fix prettier description
    
    * docs: Update project description
    
    * feat: Fix prettier error
    
    * feat: Update accessibility project description
    
    * feat: Fix prettier error

---
+++ axe-check.js
@@ -0,0 +1,53 @@
+const { AxePuppeteer } = require('axe-puppeteer');
+const path = require('path');
+const fs = require('fs');
+
+async function runAccessibilityCheck() {
+  const puppeteer = require('puppeteer');
+  const browser = await puppeteer.launch({ headless: 'new' });
+  const page = await browser.newPage();
+  const filePath = path.join(__dirname, 'index.html');
+  const fileUrl = `file://${filePath}`;
+  await page.goto(fileUrl);
+  const results = await new AxePuppeteer(page).analyze();
+  await browser.close();
+
+  const { violations } = results;
+  let report = '';
+
+  if (violations.length === 0) {
+    console.log('No accessibility violations found.');
+    report += '# ✅ No accessibility violations found.\n\n';
+  } else {
+    console.log('Accessibility violations found:');
+    report += '# Accessibility Violations\n\n';
+    for (const violation of violations) {
+      report += `## ${violation.description}\n\n`;
+      report += `[${violation.help}](${violation.helpUrl})\n\n`;
+      report += `- Impact: **${violation.impact}**\n\n`;
+      report += `- Tags: ${violation.tags.map((tag) => `\`${tag}\``).join(', ')}\n\n`;
+      let nodes = '';
+      for (const node of violation.nodes) {
+        nodes += `   1. **Node:** \`${node.html}\`, **Impact:** ${
+          node.impact
+        }\n\n       ${node.failureSummary.split('\n  ').join('\n\n      - ')}\n\n`;
+      }
+      report += `<details><summary>Click here for detailed report</summary>\n\n`;
+      report += nodes;
+      report += '</details>\n\n';
+
+      if (violation.impact === 'critical') {
+        report += '\n\n### ❌ Critical accessibility violations found.\n\nSee comments above.\n\n';
+        console.error('Critical accessibility violations found.');
+        process.exitCode = 1; // Set exit code to indicate failure
+      } else {
+        report += '\n\n### ✅ No critical accessibility violations found.\n\n';
+      }
+    }
+
+    fs.writeFileSync('accessibility_report.md', report, 'utf8');
+    console.log('Accessibility report generated.');
+  }
+}
+
+runAccessibilityCheck();
