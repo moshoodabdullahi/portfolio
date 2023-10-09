70078b602623c3cabeb51dedbe63305bbb79943f
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-10-09 23:22:35 +0100

    feat: add mobile menu

---
+++ js/index.js
@@ -0,0 +1,37 @@
+const openMenuImage = document.querySelector('#open-menu-image');
+const closeMenuImage = document.querySelector('#close-menu-image');
+const displayMenuImage = document.querySelector('#display-menu-image');
+const portfolioMenuElement = document.querySelector('#portfolio-menu-element');
+const aboutMenuElement = document.querySelector('#about-menu-element');
+const contactMenuElement = document.querySelector('#contact-menu-element');
+
+const openMenu = () => {
+  openMenuImage.classList.replace('d-block', 'd-none');
+  displayMenuImage.classList.replace('d-none', 'd-block');
+};
+
+const closeMenu = () => {
+  closeMenuImage.classList.replace('d-none', 'd-block');
+  displayMenuImage.classList.replace('d-block', 'd-none');
+};
+
+const portfolioMenu = () => {
+  portfolioMenuElement.classList.replace('d-none', 'd-block');
+  displayMenuImage.classList.replace('d-block', 'd-none');
+};
+
+const aboutMenu = () => {
+  aboutMenuElement.classList.replace('d-none', 'd-block');
+  displayMenuImage.classList.replace('d-block', 'd-none');
+};
+
+const contactMenu = () => {
+  contactMenuElement.classList.replace('d-none', 'd-block');
+  displayMenuImage.classList.replace('d-block', 'd-none');
+};
+
+openMenuImage.addEventListener('click', openMenu);
+closeMenuImage.addEventListener('click', closeMenu);
+portfolioMenuElement.addEventListener('click', portfolioMenu);
+aboutMenuElement.addEventListener('click', aboutMenu);
+contactMenuElement.addEventListener('click', contactMenu);
