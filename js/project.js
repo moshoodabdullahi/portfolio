90647d715431cd7f551a81e220fa4ddd9eedcf8e
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-11-15 11:22:14 +0100

    feat: add pop-up details to project

--- js/project.js
+++ js/project.js
@@ -1,65 +1,66 @@
-const worksTargetElement = document.querySelector('#works-target-element');
+const worksTargetElement = document.querySelector('.works-target-element');
+
+tags = ['CANOPY', 'Back End Dev', '2015'];
+links = ['html', 'Ruby on rails', 'css', 'javascript'];
 
 const projects = [
   {
     title: 'Tonic',
-    tags: [
-      `<li class="content">CANOPY</li>
-    <li class="dot"></li>
-    <li class="content">Back End Dev</li>
-    <li class="dot"></li>
-    <li class="content">2015</li>`,
-    ],
+    tags: ['CANOPY', 'Back End Dev', '2015'],
     screenshot: 'images/snapshoot-portfolio-profile-img.png',
     screenshotalt: 'Screenshot of Tonic Project',
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    links: ['html', 'Ruby on rails', 'css', 'javascript'],
   },
   {
     title: 'Multi-Post Stories',
-    tags: [
-      `<li class="content">CANOPY</li>
-    <li class="dot"></li>
-    <li class="content">Back End Dev</li>
-    <li class="dot"></li>
-    <li class="content">2015</li>`,
-    ],
+    tags: ['CANOPY', 'Back End Dev', '2015'],
     screenshot: 'images/snapshoot-portfolio-availability.png',
     screenshotalt: 'Screenshot of Multi-Post Stories Project',
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    links: ['html', 'Ruby on rails', 'css', 'javascript'],
   },
   {
     title: 'Facebook 360',
-    tags: [
-      `<li class="content">CANOPY</li>
-    <li class="dot"></li>
-    <li class="content">Back End Dev</li>
-    <li class="dot"></li>
-    <li class="content">2015</li>`,
-    ],
+    tags: ['CANOPY', 'Back End Dev', '2015'],
     screenshot: 'images/snapshoot-portfolio-weather.png',
     screenshotalt: 'Screenshot of Facebook 360 Project',
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    links: ['html', 'Ruby on rails', 'css', 'javascript'],
   },
   {
     title: 'Uber Navigation',
-    tags: [
-      `<li class="content">CANOPY</li>
-    <li class="dot"></li>
-    <li class="content">Back End Dev</li>
-    <li class="dot"></li>
-    <li class="content">2015</li>`,
-    ],
+    tags: ['CANOPY', 'Back End Dev', '2015'],
     screenshot: 'images/snapshoot-portfolio-art.png',
     screenshotalt: 'Screenshot of Uber Navigation Project',
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    links: ['html', 'Ruby on rails', 'css', 'javascript'],
+  },
+  {
+    title: 'Window Pop-up',
+    tags: ['CANOPY', 'Back End Dev', '2015'],
+    screenshot: 'images/snapshoot-portfolio-availability.png',
+    screenshotalt: 'Screenshot of Window pop-up Project',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    links: ['html', 'Ruby on rails', 'css', 'javascript'],
+  },
+  {
+    title: 'Art Work',
+    tags: ['CANOPY', 'Back End Dev', '2015'],
+    screenshot: 'images/snapshoot-portfolio-weather.png',
+    screenshotalt: 'Screenshot of Art Work Project',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    links: ['html', 'Ruby on rails', 'css', 'javascript'],
   },
 ];
 
-const generateProjectWork = (title, tags, screenshot, screenshotalt, description) =>
+const generateProjectWork = (title, tags, screenshot, screenshotalt, description, links) =>
   `<article id="tonic-element" class="work d-block d-flex-lg">
   <div class="img-profile">
     <img class="works-img" src="${screenshot}" alt="${screenshotalt}" />
@@ -68,7 +69,7 @@ const generateProjectWork = (title, tags, screenshot, screenshotalt, description
   <div class="works-article">
     <h3 class="works-headline">${title}</h3>
     <ul class="about-icon d-flex">
-      ${tags}
+    ${tags.map((tag) => '<li class="content">' + tag + '</li>').join('<li class="dot"></li>')}
     </ul>
 
     <p class="works-writeup">
@@ -76,10 +77,7 @@ const generateProjectWork = (title, tags, screenshot, screenshotalt, description
     </p>
 
     <ul class="abt-link d-flex">
-      <li><a class="works-link" href="">html</a></li>
-      <li><a class="works-link" href="">Ruby on rails</a></li>
-      <li><a class="works-link" href="">css</a></li>
-      <li><a class="works-link" href="">javascript</a></li>
+    ${links.map((link) => '<li class="works-link">' + link + '</li>').join('')}
     </ul>
 
     <button id="tonic-btn-element" class="btn">See Project</button>
@@ -94,6 +92,7 @@ const displayProjectArray = projects
       project.screenshot,
       project.screenshotalt,
       project.description,
+      project.links,
     ),
   )
   .join('');
