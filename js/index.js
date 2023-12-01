1961e1d15828aceb94db754e581f73998bbaed5f
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-12-01 11:26:40 +0100

    feat: separate project and pop-up file for better understanding

--- js/index.js
+++ js/index.js
@@ -7,27 +7,6 @@ const portfolioMenuElement = document.querySelector('#portfolio-menu-element');
 const aboutMenuElement = document.querySelector('#about-menu-element');
 const contactMenuElement = document.querySelector('#contact-menu-element');
 
-const tonicBtnElement = document.querySelector('#tonic-btn-element');
-const availabilityBtnElement = document.querySelector('#availability-btn-element');
-const artBtnElement = document.querySelector('#art-btn-element');
-const weatherBtnElement = document.querySelector('#weather-btn-element');
-const windowBtnElement = document.querySelector('#window-btn-element');
-const artWorkBtnElement = document.querySelector('#art-work-btn-element');
-
-const closeTonicElement = document.querySelector('#close-tonic-element');
-const closeAvailabilityElement = document.querySelector('#close-availability-element');
-const closeArtElement = document.querySelector('#close-art-element');
-const closeWeatherElement = document.querySelector('#close-weather-element');
-const closeWindowElement = document.querySelector('#close-window-element');
-const closeArtWorkElement = document.querySelector('#close-art-work-element');
-
-const displayTonicElement = document.querySelector('#display-tonic-element');
-const displayAvailabilityElement = document.querySelector('#display-availability-element');
-const displayWeatherElement = document.querySelector('#display-weather-element');
-const displayArtElement = document.querySelector('#display-art-element');
-const displayWindowElement = document.querySelector('#display-window-element');
-const displayArtWorkElement = document.querySelector('#display-art-work-element');
-
 const openMenu = () => {
   openMenuImage.classList.replace('d-block', 'd-none');
   displayMenuImage.classList.replace('d-none', 'd-block');
@@ -36,66 +15,6 @@ const openMenu = () => {
 const closeMenu = () => {
   closeMenuImage.classList.replace('d-none', 'd-block');
   displayMenuImage.classList.replace('d-block', 'd-none');
-};
-
-const openTonic = () => {
-  tonicBtnElement.classList.replace('d-block', 'd-none');
-  displayTonicElement.classList.replace('d-none', 'd-block');
-};
-
-const closeTonic = () => {
-  closeTonicElement.classList.replace('d-none', 'd-block');
-  displayTonicElement.classList.replace('d-block', 'd-none');
-};
-
-const openAvailability = () => {
-  availabilityBtnElement.classList.replace('d-block', 'd-none');
-  displayAvailabilityElement.classList.replace('d-none', 'd-block');
-};
-
-const closeAvailability = () => {
-  closeAvailabilityElement.classList.replace('d-none', 'd-block');
-  displayAvailabilityElement.classList.replace('d-block', 'd-none');
-};
-
-const openWeather = () => {
-  weatherBtnElement.classList.replace('d-block', 'd-none');
-  displayWeatherElement.classList.replace('d-none', 'd-block');
-};
-
-const closeWeather = () => {
-  closeWeatherElement.classList.replace('d-none', 'd-block');
-  displayWeatherElement.classList.replace('d-block', 'd-none');
-};
-
-const openArt = () => {
-  artBtnElement.classList.replace('d-block', 'd-none');
-  displayArtElement.classList.replace('d-none', 'd-block');
-};
-
-const closeArt = () => {
-  closeArtElement.classList.replace('d-none', 'd-block');
-  displayArtElement.classList.replace('d-block', 'd-none');
-};
-
-const openWindow = () => {
-  windowBtnElement.classList.replace('d-block', 'd-none');
-  displayWindowElement.classList.replace('d-none', 'd-block');
-};
-
-const closeWindow = () => {
-  closeWindowElement.classList.replace('d-none', 'd-block');
-  displayWindowElement.classList.replace('d-block', 'd-none');
-};
-
-const openArtWork = () => {
-  artWorkBtnElement.classList.replace('d-block', 'd-none');
-  displayArtWorkElement.classList.replace('d-none', 'd-block');
-};
-
-const closeArtWork = () => {
-  closeArtWorkElement.classList.replace('d-none', 'd-block');
-  displayArtWorkElement.classList.replace('d-block', 'd-none');
 };
 
 const portfolioMenu = () => {
@@ -118,21 +37,3 @@ openMenuImage.addEventListener('click', openMenu);
 portfolioMenuElement.addEventListener('click', portfolioMenu);
 aboutMenuElement.addEventListener('click', aboutMenu);
 contactMenuElement.addEventListener('click', contactMenu);
-
-tonicBtnElement.addEventListener('click', openTonic);
-closeTonicElement.addEventListener('click', closeTonic);
-
-availabilityBtnElement.addEventListener('click', openAvailability);
-closeAvailabilityElement.addEventListener('click', closeAvailability);
-
-weatherBtnElement.addEventListener('click', openWeather);
-closeWeatherElement.addEventListener('click', closeWeather);
-
-artBtnElement.addEventListener('click', openArt);
-closeArtElement.addEventListener('click', closeArt);
-
-windowBtnElement.addEventListener('click', openWindow);
-closeWindowElement.addEventListener('click', closeWindow);
-
-artWorkBtnElement.addEventListener('click', openArtWork);
-closeArtWorkElement.addEventListener('click', closeArtWork);
--- js/project.js
+++ js/project.js
@@ -1,86 +1,9 @@
+import projects from './project-list.js';
+
 const worksTargetElement = document.querySelector('#works-target-element');
 
-tags = ['CANOPY', 'Back End Dev', '2015'];
-links = ['html', 'Ruby on rails', 'css', 'javascript'];
-
-const projects = [
-  {
-    id: 'tonic-element',
-    title: 'Tonic',
-    tags: ['CANOPY', 'Back End Dev', '2015'],
-    screenshot: 'images/snapshoot-portfolio-profile-img.png',
-    screenshotalt: 'Screenshot of Tonic Project',
-    description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    links: ['html', 'Ruby on rails', 'css', 'javascript'],
-    btnId: 'tonic-btn-element',
-  },
-  {
-    id: 'availability-element',
-    title: 'Multi-Post Stories',
-    tags: ['CANOPY', 'Back End Dev', '2015'],
-    screenshot: 'images/snapshoot-portfolio-availability.png',
-    screenshotalt: 'Screenshot of Multi-Post Stories Project',
-    description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    links: ['html', 'Ruby on rails', 'css', 'javascript'],
-    btnId: 'availability-btn-element',
-  },
-  {
-    id: 'weather-element',
-    title: 'Facebook 360',
-    tags: ['CANOPY', 'Back End Dev', '2015'],
-    screenshot: 'images/snapshoot-portfolio-weather.png',
-    screenshotalt: 'Screenshot of Facebook 360 Project',
-    description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    links: ['html', 'Ruby on rails', 'css', 'javascript'],
-    btnId: 'weather-btn-element',
-  },
-  {
-    id: 'art-element',
-    title: 'Uber Navigation',
-    tags: ['CANOPY', 'Back End Dev', '2015'],
-    screenshot: 'images/snapshoot-portfolio-art.png',
-    screenshotalt: 'Screenshot of Uber Navigation Project',
-    description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    links: ['html', 'Ruby on rails', 'css', 'javascript'],
-    btnId: 'art-btn-element',
-  },
-  {
-    title: 'Window Pop-up',
-    tags: ['CANOPY', 'Back End Dev', '2015'],
-    screenshot: 'images/snapshoot-portfolio-availability.png',
-    screenshotalt: 'Screenshot of Window pop-up Project',
-    description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    links: ['html', 'Ruby on rails', 'css', 'javascript'],
-    btnId: 'window-btn-element',
-  },
-  {
-    title: 'Art Work',
-    tags: ['CANOPY', 'Back End Dev', '2015'],
-    screenshot: 'images/snapshoot-portfolio-weather.png',
-    screenshotalt: 'Screenshot of Art Work Project',
-    description:
-      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    links: ['html', 'Ruby on rails', 'css', 'javascript'],
-    btnId: 'art-work-btn-element',
-  },
-];
-
-const generateProjectWork = (
-  id,
-  title,
-  tags,
-  screenshot,
-  screenshotalt,
-  description,
-  links,
-  btnId,
-) =>
-  `<article id="${id}" class="work d-block d-flex-lg">
+const generateProjectWork = (title, tags, screenshot, screenshotalt, description, links, id) =>
+  `<article class="work d-block d-flex-lg">
   <div class="img-profile">
     <img class="works-img" src="${screenshot}" alt="${screenshotalt}" />
   </div>
@@ -99,23 +22,22 @@ const generateProjectWork = (
     ${links.map((link) => '<li class="works-link">' + link + '</li>').join('')}
     </ul>
 
-    <button id="${btnId}" class="btn">See Project</button>
+    <button id="${id}" value="see-project" class="btn">See Project</button>
   </div>
 </article>`;
 
 const displayProjectArray = projects
   .map((project) =>
     generateProjectWork(
-      project.id,
       project.title,
       project.tags,
       project.screenshot,
       project.screenshotalt,
       project.description,
-      project.links,
-      project.btnId,
+      project.link,
+      project.id,
     ),
   )
   .join('');
 
-worksTargetElement.innerHTML += displayProjectArray;
+worksTargetElement.innerHTML = displayProjectArray;
