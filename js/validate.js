f6ced7e3a874d6bd345a1d3171b85952799cb6d3
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2023-12-18 19:35:02 +0100

    feat: add formspree validation method to project

--- js/validate.js
+++ js/validate.js
@@ -1,23 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const contactForm = document.querySelector('#contact-form');
-  const emailInput = document.querySelector('#email');
-  const errorMessage = document.querySelector('#error-message');
-
-  contactForm.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    const isEmailValid = validateEmailFormat(emailInput.value);
-
-    if (isEmailValid) {
-      errorMessage.textContent = '';
-
-      console.log('Form submitted successfully!');
-    } else {
-      errorMessage.textContent = 'Email must be in lowercase.';
-    }
-  });
-
-  function validateEmailFormat(email) {
-    return email === email.toLowerCase();
-  }
-});
+const form = document.getElementById('my-form-button');
+async function handleSubmit(event) {
+  event.preventDefault();
+  const status = document.getElementById('my-form-status');
+  const data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = 'Thanks for your submission!';
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data['errors'].map((error) => error['message']).join(', ');
+          } else {
+            status.innerHTML = 'Oops! There was a problem submitting your form';
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = 'Oops! There was a problem submitting your form';
+    });
+}
+form.addEventListener('submit', handleSubmit);
