9f467ae7d1f3b67889689d6f2127ccce864a9672
Moshood Abdullahi Ayomide <111710949+moshoodabdullahi@users.noreply.github.com>
2024-01-09 18:31:25 +0100

    feat: Add Validate Form (#9)
    # This PR adds the Portfolio Project Validate Form following the design
    and layout in the [Assignment
    Link](https://github.com/bolah2009/curriculum-html-css/blob/main/portfolio/9_validate_form.md)
    and the [Figma
    Design](https://www.figma.com/file/LKueaSlr8Kc2yMKeZkKgtm/portfolio_project?type=design&node-id=48-2228&mode=design&t=SrlYt6mk7ReAfYmd-0)
    
    This milestone is used to implement a client-side validation of the data
    in the contact form before submission. and when the user submits the
    form, it checks if the email is in lowercase.

---
+++ js/validate.js
@@ -0,0 +1,45 @@
+const form = document.querySelector('#my-form');
+const emailInput = document.querySelector('#email');
+const errorMessage = document.querySelector('#error-message');
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const isEmailValid = emailInput.value === emailInput.value.toLowerCase();
+
+  if (isEmailValid) {
+    errorMessage.textContent = '';
+  } else {
+    errorMessage.textContent = 'Email must be in lowercase.';
+    return errorMessage;
+  }
+
+  const status = document.getElementById('my-form-status');
+  const data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        status.innerHTML = 'Thanks for your submission!';
+        form.reset();
+      } else {
+        response.json().then((data) => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data['errors'].map((error) => error['message']).join(', ');
+          } else {
+            status.innerHTML = 'Oops! There was a problem submitting your form';
+            return status;
+          }
+        });
+      }
+    })
+    .catch((error) => {
+      status.innerHTML = 'Oops! There was a problem submitting your form';
+    });
+}
+form.addEventListener('submit', handleSubmit);
