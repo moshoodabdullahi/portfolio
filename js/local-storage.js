7b00d9f23465ef40e897ac9511e727457dd9bf4c
Moshood Abdullahi Ayomide <moshoodabdullai442@gmail.com>
2024-01-13 07:32:03 +0100

    feat: made use of an arrow function in the local storage file

--- js/local-storage.js
+++ js/local-storage.js
@@ -2,21 +2,21 @@ const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('message');
 
-function saveFormData() {
+const saveFormData = () => {
   const formData = {
     name: nameInput.value,
     email: emailInput.value,
     message: messageInput.value,
   };
   localStorage.setItem('formData', JSON.stringify(formData));
-}
+};
 
-function loadFormData() {
+const loadFormData = () => {
   const formData = JSON.parse(localStorage.getItem('formData')) || {};
   nameInput.value = formData.name || '';
   emailInput.value = formData.email || '';
   messageInput.value = formData.message || '';
-}
+};
 
 nameInput.addEventListener('change', saveFormData);
 emailInput.addEventListener('change', saveFormData);
